bc964b8f0092d9a26ba9e0fb416a1397dc347d76
Muhammad Ali <muhammad.ali.bright@gmail.com>
2025-07-24 11:08:23 -0500

    update axios url by using env file

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,7 +1,7 @@
 // api/axios.ts
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = process.env.SERVER_PORT || "http://localhost:5000/api";
 
 export const API = axios.create({
     baseURL: API_URL,
