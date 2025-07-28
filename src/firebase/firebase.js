207915be4213a6d13183ddeae2ac2907b49e90f8
Muhammad Ali <muhammad.ali.bright@gmail.com>
2025-07-28 16:21:17 -0500

    123

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCaMBS1KuEWI0ORiZQYeL1fpcdt0QA26bA",
-    authDomain: "dentaapp-7d60f.firebaseapp.com",
-    projectId: "dentaapp-7d60f",
-    storageBucket: "dentaapp-7d60f.firebasestorage.app",
-    messagingSenderId: "376515691456",
-    appId: "1:376515691456:web:831208e0c668bef8da7e25"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
